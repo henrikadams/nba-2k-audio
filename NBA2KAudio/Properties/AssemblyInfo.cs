57115c21ae7b1bb7e9785c3236b4226ff9925e43
Leftos <leftos@gmail.com>
2013-06-18 00:58:18 +0300

    Update AssemblyInfo

--- NBA2KAudio/Properties/AssemblyInfo.cs
+++ NBA2KAudio/Properties/AssemblyInfo.cs
@@ -6,16 +6,18 @@ using System.Windows;
 
 #endregion
 
+#region Other Info
+
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("NBA2KAudio")]
+[assembly: AssemblyTitle("NBA 2K Audio Editor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NBA2KAudio")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyProduct("NBA 2K Audio Editor")]
+[assembly: AssemblyCopyright("Copyright © Eleftherios Aslanoglou & ness24 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -53,5 +55,6 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+#endregion
+
+[assembly: AssemblyVersion("0.1.0.0")]
